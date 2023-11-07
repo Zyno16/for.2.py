284342361ccceac8ef1f52e2a45a9aa80c11c33e
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-07 18:18:08 +0000

    Add files via upload

---
+++ for.2.py
@@ -0,0 +1,11 @@
+fruit = "banana"
+for i in fruit:
+    print(i)
+     # this code display hor letter if the word
+fruit = "banana"
+count = 0
+for letter in fruit:
+    if letter == "a":
+        count = count + 1
+print ("the letter a is repeated : ",count)
+    
